071f485a87879e88746e261951c9bcf8394543c0
Amir <amircb1367@gmail.com>
2025-01-16 10:18:54 +0330

    add cutomuser model and update settings and add url file to users app

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    'users.apps.UsersConfig',
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.contrib.auth import AbstractUser
+
+from django.contrib.auth.models import AbstractUser
+
 from django.core.validators import RegexValidator
+
 from django.core.exceptions import ValidationError
 
 class CustomUser(AbstractUser):
-    national_code = models.CharField('کد ملی', max_length=10, required=True)
+    national_code = models.CharField('کد ملی', max_length=10)
     birth_date = models.DateField('تاریخ تولید')
     mobile = models.CharField(
-        _("mobile"),
+        'تلفن همراه',
         max_length=11,
         unique=True,
         blank=False,
@@ -20,15 +23,15 @@ class CustomUser(AbstractUser):
         ],
         help_text="این فیلد باید دقیقاً ۱۱ کاراکتر باشد.")
     is_mobile_verified = models.BooleanField(
-        _("mobile verified"), default=False)
-    phone = models.PhoneNumberField('شماره تلفن')
+        'تایید موبایل', default=False)
+    phone = models.CharField('شماره تلفن',max_length=11)
     postal_code = models.CharField('کد پستی', max_length=10)
     use_2fa = models.BooleanField('استفاده از رمز دو عاملی', default= False)
     avatar = models.FileField('تصویر پرسنلی', upload_to='uploads/users/avatar', max_length=100)
     
     class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربرها'
 
     def __str__(self):
         return self.username    
