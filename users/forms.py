81d72ddcefd111c989b24818c9ace640ab864d6c
amir <amircb1367@gmail.com>
2025-01-17 20:11:01 +0330

    'update'

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import CustomUser
+import re
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(UserCreationForm):
@@ -22,7 +24,14 @@ class RegisterForm(UserCreationForm):
         )
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["username"].disabled = True
         self.fields["username"].widget.attrs["readonly"] = True
         self.fields['password1'].required = True
         self.fields['password2'].required = True
+    
+    
+    def clean_birthday(self):
+        data = self.cleaned_data.get('birthday')
+        r = '\d{2,4}\/\d{1,2}\/\d{1,2}'
+        if re.search(r,data):
+            raise ValidationError("فرمت تاریخ تولد اشتباه است. فرمت تاریخ باید به صورت YYYY/MM/DD باشد.")        
+        return data
--- users/views.py
+++ users/views.py
@@ -5,8 +5,10 @@ from .forms import RegisterForm
 from django.views.generic import CreateView
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
 
-
+from persiantools.jdatetime import JalaliDate
+from persiantools.jdatetime import JalaliDateTime
 
 class LogoutView(LogoutView):
     pass
@@ -20,21 +22,27 @@ class RegisterView(CreateView):
         print('1')
         user = form.save(commit=False)
         user.is_active = False
-        user.username = form.cleaned.data.get('national_code')
+        user.username = form.cleaned_data.get('national_code')
+        birthday = form.cleaned_data.get('birthday')
+        print(birthday)
+        birthday_list = birthday.split('/')
+        user.birth_date = JalaliDate(int(birthday_list[0]),int(birthday_list[1]),int(birthday_list[2])).to_gregorian()
+        print('user is', user.birth_date)
         user.save()
         print('2')
-        message = render_to_string('sms.txt',{
+        message = render_to_string('users/sms.txt',{
             'otp':12334
         })
         print('3')
         to_email = form.cleaned_data.get('email')
-        to_mobile = form.cleaned.data.get('mobile')
+        to_mobile = form.cleaned_data.get('mobile')
         print(to_email)
-        email = EmailMessage(
-            'sms email', message, to=[to_email]
-        )
+        if to_email:
+            email = EmailMessage(
+                'sms email', message, to=[to_email]
+            )
+            email.send()
         print(to_email)
-        email.send()
         print('ok')
         return HttpRequest('regster seccessfull')
 
