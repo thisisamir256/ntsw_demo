4c621b54e9334513388c905c2b6db593a4f7b5af
Amir <amircb1367@gmail.com>
2025-01-20 18:08:07 +0330

    add base and business dashboard url ; ;

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import BaseRoleView, BusinessRoleView
 
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path("", home, name="home"),
-
-
+    path('base/', BaseRoleView.as_view(), name='base_role_dashboard'),
+    path('business/', BusinessRoleView.as_view(),
+         name="business_role_dashboard"),
 ]
