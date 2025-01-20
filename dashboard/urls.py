012077a97f1fe31ce60a935125a26e77879eec35
Amir <amircb1367@gmail.com>
2025-01-20 18:28:26 +0330

    add home menue to role sidebars

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BaseRoleView, BusinessRoleView
+from .views import BaseRoleView, BusinessRoleView, TraderRoleView
 
 
 app_name = 'dashboard'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('base/', BaseRoleView.as_view(), name='base_role_dashboard'),
     path('business/', BusinessRoleView.as_view(),
          name="business_role_dashboard"),
+    path('trader/', TraderRoleView.as_view(), name='trader_role_dashbord')
 ]
