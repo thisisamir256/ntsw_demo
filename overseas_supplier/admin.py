5b5107e0fead5c78f8227a74b002fd0bc1215791
Amir <amircb1367@gmail.com>
2025-01-21 16:10:32 +0330

    add models to admin

--- overseas_supplier/admin.py
+++ overseas_supplier/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Person, Company, DocumentType
 
-# Register your models here.
+
+@admin.register(DocumentType)
+class DocumentTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DocumentType)
+class DocumentTypeAdmin(admin.ModelAdmin):
+    pass
--- overseas_supplier/views.py
+++ overseas_supplier/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .models import Person, Company
+User = get_user_model()
+
+
+class OverSeasSuplierListView(View):
+    def get(self):
+        p_q = Person.objects.filter()
