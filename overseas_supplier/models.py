b11df462c1fabf407c970efbe891dad63f4f767a
Amir <amircb1367@gmail.com>
2025-01-21 15:33:28 +0330

    add overseas_suplier tables

--- overseas_supplier/models.py
+++ overseas_supplier/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
-# Create your models here.
+
+class DocumentType(models.Model):
+    name = models.CharField('نام', max_length=50)
+
+    class Meta:
+        verbose_name = 'نوع مدرک شناسایی'
+        verbose_name_plural = 'نوع مدرک شناسایی'
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    SEX_CHOISES = {
+        'mail': 'مرد',
+        'fmail': 'زن'
+    }
+    MARIED_CHOISES = {
+        's': 'مجرد',
+        'm': 'متاهل',
+        'c': 'متارکه',
+        'w': 'بیوه',
+    }
+
+    first_name = models.CharField('نام/ First Name', max_length=100)
+    last_name = models.CharField('نام خانوادگی/ Last Name', max_length=100)
+    father_name = models.CharField('نام پدر', max_length=100)
+    mother_name = models.CharField(
+        'نام مادر',
+        max_length=100,
+        default=None,
+        blank=True
+    )
+    grandfather_name = models.CharField(
+        'نام جد',
+        max_length=100,
+        blank=True,
+        default=None
+    )
+    country = models.CharField(
+        'کشور محل تولید', max_length=50)
+    city = models.CharField('شهر محل تولد', max_length=50,
+                            default=None,  blank=True)
+    sex = models.CharField('جنسیت', choices=SEX_CHOISES, max_length=5)
+    birthday = models.DateField('تاریخ تولید')
+    nationality = models.CharField('ملیت', max_length=50)
+    maried = models.CharField('وضعیت تاهل', max_length=50)  # tod: add choises
+    personal_image = models.ImageField(
+        'تصویر پرسنلی',
+        upload_to='/uploads/overseas_supplier/',
+        max_length=100
+    )
+    document_type = models.ForeignKey(
+        DocumentType,
+        verbose_name='نوع مدرک شناسایی',
+        on_delete=models.CASCADE
+    )
+    document_number = models.CharField('شماره مدرک شناسایی', max_length=100)
+    create_date = models.DateField('تاریخ صدور')
+    expire_date = models.DateField('تاریخ انقضا')
+    # todo: add document image field
+
+    class Meta:
+        verbose_name = 'شخص حقیقی'
+        verbose_name_plural = 'شخص حقیقی'
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+
+    name = models.CharField('نام فارسی شرکت خارجی', max_length=100)
+    en_name = models.CharField('نام لاتین شرکت خارجی', max_length=100)
+    register_number = models.PositiveSmallIntegerField('شماره ثبت شرکت')
+    company_type = models.CharField('نوع شرکت', max_length=50)
+    registered_country = models.CharField('کشور محل ثبت', max_length=50)
+    registered_date = models.DateField('تاریخ ثبت')
+    registered_from = models.CharField('تابعیت ثبتی شرکت', max_length=50)
+    owner_type = models.CharField('نوع مالکیت', max_length=50)
+    country = models.CharField('کشور', max_length=50)
+    city = models.CharField('شهر', max_length=50)
+    address = models.TextField('آدرس')
+    postal_code = models.PositiveSmallIntegerField('کد پستی')
+    phone = models.CharField('شماره تلفن', max_length=20)
+
+    class Meta:
+        verbose_name = _("company")
+        verbose_name_plural = _("companys")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("company_detail", kwargs={"pk": self.pk})
