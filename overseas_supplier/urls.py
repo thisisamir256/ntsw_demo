3bb0c06b66488a89fa743331110b216e032f3882
Amir <amircb1367@gmail.com>
2025-01-22 16:50:01 +0330

    change create url name

--- overseas_supplier/urls.py
+++ overseas_supplier/urls.py
@@ -5,5 +5,5 @@ app_name = 'supplier'
 
 urlpatterns = [
     path('', OverSeasSuplierListView.as_view(), name='supplier_list'),
-    path('create', PersonCreateView.as_view(), name='create_supplier'),
+    path('create', PersonCreateView.as_view(), name='create_person'),
 ]
