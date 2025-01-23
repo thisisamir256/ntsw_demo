df556868016f6dfc26eddb15981532bf08806946
Amir <amircb1367@gmail.com>
2025-01-23 18:22:22 +0330

    create order_registration app

---
+++ order_registration/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OrderRegistrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'order_registration'
+    verbose_name = 'ثبت سفارش'
