2f9b52dc44a50e404f71207af3317bfe0a3b6ccd
Amir <amircb1367@gmail.com>
2025-01-25 17:30:04 +0330

    update

--- order_registration/forms.py
+++ order_registration/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import MainData
+from overseas_supplier.models import Person, Company
 
 
 class MainDataForm(forms.ModelForm):
+    choices = [
+        ('person', 'حقیقی'),
+        ('company', 'حقوقی')
+    ]
+    related_object_id = forms.IntegerField()
 
     class Meta:
         model = MainData
@@ -12,8 +18,22 @@ class MainDataForm(forms.ModelForm):
             'proforma_invoice_issue_date',
             'proforam_invoice_expire_date',
             'order_registration_case',
-            # 'supplier_type',
-            # 'object_id',
-            # 'related_object',
             'producer_type',
         )
+
+        def save(self, commit=True):
+            instance = super.save(commit=False)
+            supplier_type = self.cleaned_data['supplier_type']
+            object_id = self.cleaned_data['related_object_id']
+
+            if supplier_type == 'person':
+                instance.supplier_type = ContentType.objects.get_for_model(
+                    Person)
+            elif supplier_type == 'company':
+                instance.supplier_type = ContentType.objects.get_for_model(
+                    Company)
+            instance.object_id = object_id
+
+            if commit:
+                instance.save()
+            return instance
--- order_registration/views.py
+++ order_registration/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import MainDataForm
+from django.urls import reverse_lazy
 
 
 class MainDataCreateView(LoginRequiredMixin, CreateView):
     form_class = MainDataForm
     template_name = 'order_registration/create_main_data.html'
+    success_url = reverse_lazy('users:login')
